841ee20342afa2df2de382341e3aada27bdd0c30
Florencia Barrios <fbarrios1@uc.cl>
2018-10-18 15:20:32 -0300

    Added service model

--- common/models/person.js
+++ common/models/person.js
@@ -1,5 +1,22 @@
 'use strict';
+var app = require('../../server/server.js');
 
 module.exports = function(Person) {
-
+  // role assingment
+  Person.observe('after save', function(context, next) {
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+    Role.findOne({
+      where: {name: context.instance.accountType},
+    }, function(err, role) {
+      if (context.isNewInstance) {
+        RoleMapping.create({
+          principalType: RoleMapping.USER,
+          principalId: context.instance.id,
+          roleId: role.id,
+        });
+      };
+      next();
+    });
+  })
 };
