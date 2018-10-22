2c419bf6565d8862d330b84dcea4eb73d73b94ff
Jose Morales <jmmorales@uc.cl>
2018-10-21 22:50:56 -0300

    Disable routes

--- common/models/person.js
+++ common/models/person.js
@@ -2,6 +2,57 @@
 var app = require('../../server/server.js');
 
 module.exports = function(Person) {
+  // Hide methods
+  Person.disableRemoteMethodByName('patchOrCreate');
+  Person.disableRemoteMethodByName('replaceOrCreate');
+  Person.disableRemoteMethodByName('prototype.__create__accessTokens');
+  Person.disableRemoteMethodByName('prototype.__get__accessTokens');
+  Person.disableRemoteMethodByName('prototype.__delete__accessTokens');
+  Person.disableRemoteMethodByName('prototype.__findById__accessTokens');
+  Person.disableRemoteMethodByName('prototype.__updateById__accessTokens');
+  Person.disableRemoteMethodByName('prototype.__destroyById__accessTokens');
+  Person.disableRemoteMethodByName('prototype.__count__accessTokens');
+  Person.disableRemoteMethodByName('exists');
+  Person.disableRemoteMethodByName('count');
+  Person.disableRemoteMethodByName('findOne');
+  Person.disableRemoteMethodByName('replaceById');
+  Person.disableRemoteMethodByName('prototype.verify');
+  Person.disableRemoteMethodByName('resetPassword');
+  Person.disableRemoteMethodByName('setPassword');
+  Person.disableRemoteMethodByName('updateAll');
+  Person.disableRemoteMethodByName('upsertWithWhere');
+  Person.disableRemoteMethodByName('createChangeStream');
+  Person.disableRemoteMethodByName('createChangeStream');
+  Person.disableRemoteMethodByName('confirm');
+  Person.disableRemoteMethodByName('prototype.__get__messages');
+  Person.disableRemoteMethodByName('prototype.__create__messages');
+  Person.disableRemoteMethodByName('prototype.__delete__messages');
+  Person.disableRemoteMethodByName('prototype.__findById__messages');
+  Person.disableRemoteMethodByName('prototype.__updateById__messages');
+  Person.disableRemoteMethodByName('prototype.__destroyById__messages');
+  Person.disableRemoteMethodByName('prototype.__count__messages');
+  Person.disableRemoteMethodByName('prototype.__get__posts');
+  Person.disableRemoteMethodByName('prototype.__create__posts');
+  Person.disableRemoteMethodByName('prototype.__delete__posts');
+  Person.disableRemoteMethodByName('prototype.__findById__posts');
+  Person.disableRemoteMethodByName('prototype.__updateById__posts');
+  Person.disableRemoteMethodByName('prototype.__destroyById__posts');
+  Person.disableRemoteMethodByName('prototype.__count__posts');
+  Person.disableRemoteMethodByName('prototype.__get__responses');
+  Person.disableRemoteMethodByName('prototype.__create__responses');
+  Person.disableRemoteMethodByName('prototype.__delete__responses');
+  Person.disableRemoteMethodByName('prototype.__findById__responses');
+  Person.disableRemoteMethodByName('prototype.__updateById__responses');
+  Person.disableRemoteMethodByName('prototype.__destroyById__responses');
+  Person.disableRemoteMethodByName('prototype.__count__responses');
+  Person.disableRemoteMethodByName('prototype.__get__subscriptions');
+  Person.disableRemoteMethodByName('prototype.__create__subscriptions');
+  Person.disableRemoteMethodByName('prototype.__delete__subscriptions');
+  Person.disableRemoteMethodByName('prototype.__findById__subscriptions');
+  Person.disableRemoteMethodByName('prototype.__updateById__subscriptions');
+  Person.disableRemoteMethodByName('prototype.__destroyById__subscriptions');
+  Person.disableRemoteMethodByName('prototype.__count__subscriptions');
+
   // role assingment
   Person.observe('after save', function(context, next) {
     var Role = app.models.Role;
--- common/models/service.js
+++ common/models/service.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function(Service) {
+  // Hide methods
+  Service.disableRemoteMethodByName('patchOrCreate');
+  Service.disableRemoteMethodByName('replaceOrCreate');
+  Service.disableRemoteMethodByName('exists');
+  Service.disableRemoteMethodByName('count');
+  Service.disableRemoteMethodByName('findOne');
+  Service.disableRemoteMethodByName('replaceById');
+  Service.disableRemoteMethodByName('updateAll');
+  Service.disableRemoteMethodByName('upsertWithWhere');
+  Service.disableRemoteMethodByName('createChangeStream');
+  Service.disableRemoteMethodByName('createChangeStream');
 
 };
