e310adadb48ae359da0d91c8d707f649e210f46e
jedelacarrera <jedelacarrera@uc.cl>
2018-11-13 12:51:12 -0300

    :star: update notification value of subscription when a new message is created

--- common/models/message.js
+++ common/models/message.js
@@ -2,9 +2,26 @@
 
 module.exports = function(Message) {
   Message.observe('before save', function(context, next) {
-  	if (context.options.accessToken) {
-    	context.instance.personId = context.options.accessToken.userId;
-  	}
+    if (context.options.accessToken) {
+      context.instance.personId = context.options.accessToken.userId;
+    }
     next();
+  });
+
+  Message.observe('after save', function(context, next) {
+    var postId = context.instance.postId;
+    Message.app.models.Subscription.updateAll(
+      {
+        postId: context.instance.postId,
+        notification: false,
+      },
+      {
+        notification: true,
+      },
+      function(err) {
+        if (err) console.log(err);
+        next();
+      }
+    );
   });
 };
